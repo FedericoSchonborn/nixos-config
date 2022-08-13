993eb7be229dd6dfa3977a7072139e8c8f627d44
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-08-13 05:54:13 -0300

    Enable nix-direnv
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- users/federico/modules/programs/direnv.nix
+++ users/federico/modules/programs/direnv.nix
@@ -1,7 +1,9 @@
 {
   programs.direnv = {
     enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
+    nix-direnv = {
+      enable = true;
+      enableFlakes = true;
+    };
   };
 }
