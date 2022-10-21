9d520ad262b02c670b4d67754abfbfc828a32a97
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-10-20 21:31:28 -0300

    swift3: Update install.sh
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- hosts/swift3/install.sh
+++ hosts/swift3/install.sh
@@ -7,7 +7,4 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
-git clone --depth 1 https://github.com/FedericoSchonborn/nixos-config
-pushd nixos-config
 nixos-install --verbose --flake ".#swift3" --no-channel-copy --no-root-password
-popd
