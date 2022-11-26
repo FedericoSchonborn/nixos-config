d94b12dff3c3fb8cfd72eae40de8bcacfe025b91
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-11-25 23:49:41 -0300

    Add default search engine
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- users/federico/programs/firefox/profiles.nix
+++ users/federico/programs/firefox/profiles.nix
@@ -1,44 +1,47 @@
 {pkgs, ...}: {
   programs.firefox.profiles.default = {
-    search.engines = {
-      "NixOS Packages" = {
-        urls = [
-          {
-            template = "https://search.nixos.org/packages";
-            params = [
-              {
-                name = "channel";
-                value = "unstable";
-              }
-              {
-                name = "query";
-                value = "{searchTerms}";
-              }
-            ];
-          }
-        ];
-        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-        definedAliases = ["@nixpkgs"];
+    search = {
+      engines = {
+        "NixOS Packages" = {
+          urls = [
+            {
+              template = "https://search.nixos.org/packages";
+              params = [
+                {
+                  name = "channel";
+                  value = "unstable";
+                }
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = ["@nixpkgs"];
+        };
+        "NixOS Options" = {
+          urls = [
+            {
+              template = "https://search.nixos.org/options";
+              params = [
+                {
+                  name = "channel";
+                  value = "unstable";
+                }
+                {
+                  name = "query";
+                  value = "{searchTerms}";
+                }
+              ];
+            }
+          ];
+          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          definedAliases = ["@nixopts"];
+        };
       };
-      "NixOS Options" = {
-        urls = [
-          {
-            template = "https://search.nixos.org/options";
-            params = [
-              {
-                name = "channel";
-                value = "unstable";
-              }
-              {
-                name = "query";
-                value = "{searchTerms}";
-              }
-            ];
-          }
-        ];
-        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-        definedAliases = ["@nixopts"];
-      };
+      default = "Google";
     };
   };
 }
